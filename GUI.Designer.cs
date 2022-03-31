98c9ff1de4b25c40f628726ea0cf7d954f9e09e6
Leon Niklas Kelle <42148131+master4x@users.noreply.github.com>
2022-03-31 13:42:33 +0200

    Remove German Comments

--- GUI.Designer.cs
+++ GUI.Designer.cs
@@ -3,15 +3,8 @@ namespace LTB_Verwaltung
 {
     partial class GUI
     {
-        /// <summary>
-        /// Erforderliche Designervariable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Verwendete Ressourcen bereinigen.
-        /// </summary>
-        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -23,10 +16,6 @@ namespace LTB_Verwaltung
 
         #region Vom Windows Form-Designer generierter Code
 
-        /// <summary>
-        /// Erforderliche Methode für die Designerunterstützung.
-        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
-        /// </summary>
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUI));
